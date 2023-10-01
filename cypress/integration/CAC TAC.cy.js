d5cbf78d5fa37bf3e373c7244f9c5db0515ebf92
Jussara Rosa Ferreira <jussarar308@gmail.com>
2023-10-01 11:44:18 -0300

    Initial project
    Initial project

---
+++ cypress/integration/CAC TAC.cy.js
@@ -0,0 +1,14 @@
+// untitled.spec.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+
+///reference types-"Cypress" />
+
+describe ('Central de atendimento ao cliente TAT', ()=>{
+    it('verifica o titulo da aplicação', ()=>{
+        cy.visit('./src/index.html')
+    })
+})
